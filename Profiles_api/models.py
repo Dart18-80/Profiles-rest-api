36bf3107ccedd3ccb28c620b806e5cf6a0d1b434
Dart <diegoruiztrabanino13@gmail.com>
2021-11-18 15:21:53 -0600

    Solucion de problema con sintaxis y libreria

--- Profiles_api/models.py
+++ Profiles_api/models.py
@@ -4,22 +4,24 @@ from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager
 
 
-"Controlador de Perfiles para los usuarios"
 class UserProfileManager(BaseUserManager):
-    def create_user(self, email, name, password=none):
+    def create_user(self, email, name, password=None):
         if not email:
-            raise ValueError('Se debe agregar el Correo Electronico')
+            raise ValueError("Users must have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(email= email, name= name)
+        user = self.model(
+            email=email,
+            name=name,
+        )
+
         user.set_password(password)
-        user.save(using= self._db)
+        user.save(using=self._db)
 
         return user
 
-    "Guardar y crear un super Usuario"
     def create_superuser(self, email, name, password):
-        user = self.create_use(email,name, password)
+        user = self.create_user(email, name, password)
 
         user.is_superuser = True
         user.is_staff = True
@@ -28,27 +30,21 @@ class UserProfileManager(BaseUserManager):
         return user
 
 
-# Create your models here.
-"""Modelo base de datos para los usuarios """
 class UserProfile(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_activate = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    objects = UserProfileManager()
 
-    objects = UserProfilesManager()
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
 
-
-    "Funcion de Retorno de nombre completo"
     def get_full_name(self):
         return self.name
 
-    "Funcion de Retorno de nombre pequeño"
     def get_short_name(self):
         return self.name
 
-    "Funcion de Retorno de Email"
-    def _str_(self):
+    def __str__(self):
         return self.email
--- Profiles_project/settings.py
+++ Profiles_project/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Profiles_project.urls'
+ROOT_URLCONF = 'profiles_project.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Profiles_project.wsgi.application'
+WSGI_APPLICATION = 'profiles_project.wsgi.application'
 
 
 # Database
