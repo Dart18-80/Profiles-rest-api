6edfdea64b6dabf0d7b59154eb814b48c3b67919
Dart <diegoruiztrabanino13@gmail.com>
2021-11-19 09:19:16 -0600

    Metodos de api

--- Profiles_api/views.py
+++ Profiles_api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from Profiles_api import serializers
 
-# Create your views here.
+""" Declaracion de las vistas API """
+
+class HelloApiView(APIView):
+    serializer_class = serializers.HelloSerializer
+
+    def get(self, request, format=None):
+        an_apiview =[
+            'Uses HTTP methods as function(get, post, patch, put, delete)',
+            'Is similar to a traditional Django View',
+            'Gives you the most control over you application logic',
+            'Is mapped manually to URLs'
+        ]
+        return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def put(self, request, pk=None):
+        return Response({'method':'PUT'})
+
+    def patch(self, request, pk=None):
+        return Response({'method': 'PATCH'})
+
+    def delete(self, request, pk=None):
+        return Response({'method': 'DELETE'})
