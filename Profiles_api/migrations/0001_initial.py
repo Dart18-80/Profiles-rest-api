d42d9961c1ba3d1fdaf5ca96e1f2fbf949895eef
Dart18-80 <diegoruiztrabanino13@gmail.com>
2021-12-15 09:28:48 -0600

    Error en la base de datos 5

--- Profiles_api/migrations/0001_initial.py
+++ Profiles_api/migrations/0001_initial.py
@@ -9,6 +9,10 @@ class Migration(migrations.Migration):
 
     initial = True
 
+    dependencies = [
+        ('auth', '0012_alter_user_first_name_max_length'),
+    ]
+
     operations = [
         migrations.CreateModel(
             name='UserProfile',
@@ -21,8 +25,8 @@ class Migration(migrations.Migration):
                 ('name', models.CharField(max_length=255)),
                 ('is_active', models.BooleanField(default=True)),
                 ('is_staff', models.BooleanField(default=False)),
-                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', verbose_name='groups')),
-                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', verbose_name='user permissions')),
+                ('groups', models.ManyToManyField(blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', related_name='user_set', related_query_name='user', to='auth.Group', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(blank=True, help_text='Specific permissions for this user.', related_name='user_set', related_query_name='user', to='auth.Permission', verbose_name='user permissions')),
             ],
             options={
                 'abstract': False,
